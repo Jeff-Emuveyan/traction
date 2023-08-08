b98562a6b0901359f73ebf5308df4d3cf8e67b4e
jeff <jeff.emuveyan@coosto.com>
2023-08-08 23:43:26 +0100

    added Outlined text field

--- features/movies/src/main/java/com/example/movies/ui/MoviesScreen.kt
+++ features/movies/src/main/java/com/example/movies/ui/MoviesScreen.kt
@@ -1,5 +1,6 @@
 package com.example.movies.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,11 +20,20 @@ import androidx.compose.material.pullrefresh.PullRefreshState
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -62,11 +72,25 @@ internal fun MoviesScreen(
     onRetry:() -> Unit) {
 
     Box(modifier = Modifier.pullRefresh(refreshingState)) {
+        var text by remember { mutableStateOf("") }
 
         PullRefreshIndicator(refreshing, refreshingState, modifier = Modifier
             .align(Alignment.TopCenter))
 
-        MovieList(listState = listState, list)
+        Column {
+            OutlinedTextField(
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.White,
+                ),
+                modifier = Modifier.fillMaxWidth(),
+                value = text,
+                onValueChange = { text = it },
+                singleLine = true,
+                label = { Text("Search by title") }
+            )
+            MovieList(listState = listState, list)
+        }
     }
 }
 
